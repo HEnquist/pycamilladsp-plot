6da3c9004c15c0eb255d37cdba23e2b8b987dada
Henrik Enquist <henrik.enquist@gmail.com>
2022-09-23 21:41:22 +0200

    Bump to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="camilladsp_plot",
-    version="1.0.1",
+    version="1.0.2",
     author="Henrik Enquist",
     author_email="henrik.enquist@gmail.com",
     description="A library for validating, evaluating and plotting configs and filters for CamillaDSP",
